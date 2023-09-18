01611f06f576c80c298c7879ea94957659ace7f4
rpatil5 <rishistar05@gmail.com>
2023-09-17 23:49:15 -0500

    Checking port

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,7 @@ import cors from "cors"
 const app = express()
 
 app.use(cors({
-    origin: "http://localhost:63342",
+    origin: "http://localhost:5500",
     credentials:true,            //access-control-allow-credentials:true
     optionSuccessStatus:200
 }))
