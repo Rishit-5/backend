30c4ceec4b49d9ff94a6cc32bf15a5b2a167aea9
rpatil5 <rishistar05@gmail.com>
2023-09-17 22:26:36 -0500

    project progress

--- backend/index.js
+++ backend/index.js
@@ -4,16 +4,43 @@ import cors from "cors"
 const app = express()
 
 app.use(cors({
-    origin: "http://localhost:5500"
+    origin: "http://localhost:63342",
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
 }))
 
-app.get("/", (req, res) => {
-    res.send({
-        x: [1, 2, 3],
-        y: [4, 5, 6]
-    }).status(200)
+app.get("/", async (req, res) => {
+    var requestOptions = {
+        method: 'GET',
+        redirect: 'follow'
+    };
+    let years = []
+    let stats = []
+
+    fetch("https://nba-stats-db.herokuapp.com/api/playerdata/name/Lebron James", requestOptions)
+        .then(response => response.json())
+        .then(result => {
+            // console.log(result.results)
+            console.log(result.results.length)
+            for (let i = 0; i < result.results.length; i++) {
+                stats.push(result.results[i].three_fg/result.results[i].games)
+                years.push(result.results[i].season)
+
+            }
+                // console.log(stats)
+            // for (let i = 0; i < stats.length; i++) {
+            //     years.push(i+1)
+            // }
+            }
+        ).then(result =>
+        res.send({
+            x: years,
+            y: stats
+        }).status(200)
+    )
+        .catch(error => console.log('error', error));
 })
 
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 10000
 app.listen(port, () => console.log(`server listening on port ${port} 🚀`))
