7d2be10abd99a55ae9834c57ef77a563baaf932c
rpatil5 <rishistar05@gmail.com>
2023-09-17 23:22:06 -0500

    port changed one last time

--- frontend/graph.mjs
+++ frontend/graph.mjs
@@ -2,7 +2,7 @@ const graphDiv = document.getElementById("graph");
 
 fetch(
     // https://oa-2023-24-backend.onrender.com
-    "https://dssd-backend.onrender.com" //use "http://localhost:3000" if running sample express backend locally, or replace with your own backend endpoint url
+    "https://gorgeous-pink-galoshes.cyclic.cloud/" //use "http://localhost:3000" if running sample express backend locally, or replace with your own backend endpoint url
 ).then(async res => {
     Plotly.newPlot( graphDiv, [ await res.json() ]);
 })
