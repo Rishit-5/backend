35b1c4e185d183e4c9fa3d748586d5029a2745a3
rpatil5 <rishistar05@gmail.com>
2023-09-18 02:10:27 -0500

    Stat Options + Better UI

--- backend/index.js
+++ backend/index.js
@@ -4,12 +4,12 @@ import cors from "cors"
 const app = express()
 
 app.use(cors({
-    origin: "http://localhost:63342",
+    origin: "http://localhost:5500",
     credentials:true,            //access-control-allow-credentials:true
     optionSuccessStatus:200
 }))
 
-app.get("/:name", async (req, res) => {
+app.get("/:name/:stat", async (req, res) => {
     var requestOptions = {
         method: 'GET',
         redirect: 'follow'
@@ -17,14 +17,30 @@ app.get("/:name", async (req, res) => {
     let years = []
     let stats = []
     const name = req.params.name;
+    const stat = req.params.stat;
+    console.log(name);
+    console.log(stat);
     // console.log(name)
     fetch("https://nba-stats-db.herokuapp.com/api/playerdata/name/" + name, requestOptions)
         .then(response => response.json())
         .then(result => {
+            let ans;
+
             // console.log(result.results)
             console.log(result.results.length)
             for (let i = 0; i < result.results.length; i++) {
-                stats.push(result.results[i].three_fg/result.results[i].games)
+                if (stat == "Points"){
+                    ans  = result.results[i].PTS/result.results[i].games;
+                } else if (stat == "3s") {
+                    ans  = result.results[i].three_fg/result.results[i].games;
+                } else if (stat == "Minutes") {
+                    ans  = result.results[i].minutes_played/result.results[i].games;
+                }else if (stat == "Assists") {
+                    ans  = result.results[i].AST/result.results[i].games;
+                } else if (stat == "Rebounds") {
+                    ans  = result.results[i].TRB/result.results[i].games;
+                }
+                stats.push(ans)
                 years.push(result.results[i].season)
 
             }
--- frontend/graph.mjs
+++ frontend/graph.mjs
@@ -1,10 +1,17 @@
 const graphDiv = document.getElementById("graph");
+let player = "Stephen Curry"
+let stat;
+document.getElementById("Submit").onclick = function(){
+    player = document.getElementById("player").value;
+    stat = document.getElementById("stat").value;
 
-fetch(
-    // https://oa-2023-24-backend.onrender.com
-     "https://gorgeous-pink-galoshes.cyclic.cloud/" //use "http://localhost:3000" if running sample express backend locally, or replace with your own backend endpoint url
-    // "http://localhost:3000/" + "Stephen Curry"
-).then(async res => {
-    Plotly.newPlot( graphDiv, [ await res.json() ]);
-})
+    fetch(
+        // https://oa-2023-24-backend.onrender.com
+        "https://gorgeous-pink-galoshes.cyclic.cloud/" + player + "/" + stat  //use "http://localhost:3000" if running sample express backend locally, or replace with your own backend endpoint url
+         // "http://localhost:3000/" + player + "/" + stat
+    ).then(async res => {
+        Plotly.newPlot( graphDiv, [ await res.json() ]);
+    })
+}
+
 
